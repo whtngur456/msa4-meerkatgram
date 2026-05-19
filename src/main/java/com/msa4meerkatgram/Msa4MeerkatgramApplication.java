82a2acec8d51151a366d7a905eed49ff4535dc47
whtngur456 <p99344786@gmail.com>
2026-05-19 15:45:02 +0900

    260519 [develop] init

---
+++ src/main/java/com/msa4meerkatgram/Msa4MeerkatgramApplication.java
@@ -0,0 +1,13 @@
+package com.msa4meerkatgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msa4MeerkatgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4MeerkatgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msa4meerkatgram/Msa4MeerkatgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa4meerkatgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msa4MeerkatgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
